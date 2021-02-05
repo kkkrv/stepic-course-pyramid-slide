1645a97343c9c8ca5817107bde417d13fa1ddc86
Olga Kokkareva <olgakokkareva08055@gmail.com>
2021-02-05 22:16:13 +0300

    add listeners

---
+++ mario.js
@@ -0,0 +1,44 @@
+const brickField = document.getElementById("brick");
+brickField.addEventListener("change", (event) => {
+    let size = getSize();
+    let brick = event.target.value;
+    drawPyramid(brick, size);
+})
+
+const sizeField = document.getElementById("size");
+sizeField.addEventListener("change", (event) => {
+    let size = Number(event.target.value);
+    console.log("SIZE: " + size);
+    let brick = getBrick();
+    drawPyramid(brick, size);
+})
+
+function getBrick() {
+    const brickField = document.getElementById("brick");
+    return brickField.value;
+}
+
+function getSize() {
+    const sizeField = document.getElementById("size");
+    return Number(sizeField.value);
+}
+
+function drawPyramid(brick = "#", size = 5) {
+
+    const container = document.getElementById('pyramid');
+    container.innerHTML = '';
+    let result = ``;
+    for (let i = 2; i <= size ; i++) {
+        let row = brick;
+        row = row.repeat(i);
+        row = row.padStart(size, `\xa0`);
+        result = result + row + "<br>";
+    }
+    let pyramid = document.createElement("div");
+    pyramid.innerHTML = result;
+    container.appendChild(pyramid);
+    console.log("Size: " + size + " Brick: " + brick);
+
+}
+
+drawPyramid("#", 3)
